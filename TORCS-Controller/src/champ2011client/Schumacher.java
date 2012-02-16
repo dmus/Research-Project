18f7a755e040d9a65121d784136ecf4b6cca160a
dmus <derkmus@gmail.com>
2012-02-16 14:15:34 +0100

    First unsuccessful trial executed

--- TORCS-Controller/src/champ2011client/Schumacher.java
+++ TORCS-Controller/src/champ2011client/Schumacher.java
@@ -1,5 +1,16 @@
 package champ2011client;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.commons.math.linear.Array2DRowRealMatrix;
+import org.apache.commons.math.linear.ArrayRealVector;
+import org.apache.commons.math.linear.RealMatrix;
+import org.apache.commons.math.linear.RealVector;
+
 import champ2011client.behaviour.*;
 
 /**
@@ -14,10 +25,45 @@ public class Schumacher extends Controller {
     protected StandardGearChangeBehaviour gearBehaviour = new StandardGearChangeBehaviour();
     protected ClutchBehaviour clutchBehaviour = new ClutchBehaviour();
     
-    protected float[][] actions = new float[15][2];
+    protected RealMatrix a, b, actions;
+    protected RealVector theta;
     
-    public float getValueForNextState(SensorModel s, float[][] a) {
-    	return 0.0f;
+    public Schumacher() throws FileNotFoundException {
+    	double[][] dataA = readMatrixFromCsv("../TORCS-Dynamics/A.csv");
+    	double[][] dataB = readMatrixFromCsv("../TORCS-Dynamics/B.csv");
+    	double[][] dataTheta = readMatrixFromCsv("../TORCS-Dynamics/theta.csv");
+    	double[][] dataActions = readMatrixFromCsv("../TORCS-Dynamics/actions.csv");
+    	
+    	a = new Array2DRowRealMatrix(dataA);
+    	b = new Array2DRowRealMatrix(dataB);
+    	theta = new Array2DRowRealMatrix(dataTheta).getColumnVector(0);
+    	actions = new Array2DRowRealMatrix(dataActions);
+    }
+    
+    protected double[][] readMatrixFromCsv(String filename) throws FileNotFoundException {
+    	List<String> data = new ArrayList<String>();
+    	FileReader fin = new FileReader(filename);
+    	Scanner sc = new Scanner(fin);
+    	while(sc.hasNext()) {
+    		data.add(sc.nextLine());
+    	}
+    	sc.close();
+
+    	double[][] matrix = new double[data.size()][];
+    	int count = -1;
+    	for(String s : data) {
+    		String[] temp = data.get(++count).split(",");
+    		matrix[count] = new double[temp.length];
+    		for(int i = 0; i < temp.length; i++) {
+    			System.out.println(temp[i]);
+    			System.out.println(Double.valueOf(temp[i]));
+    			
+    			matrix[count][i] = Double.valueOf(temp[i]);
+    			System.out.println("done");
+    		}
+    	}
+    	
+    	return matrix;
     }
     
     public Action control(SensorModel sensorModel) {
@@ -35,8 +81,37 @@ public class Schumacher extends Controller {
         }
         */
         
-        float velocity = 0;
-        float steering = 0;
+        double[] dataS = {
+        		sensorModel.getDistanceFromStartLine(),
+        		sensorModel.getTrackPosition(),
+        		sensorModel.getAngleToTrackAxis(),
+        		sensorModel.getSpeed(),
+        		sensorModel.getLateralSpeed()
+        };
+        
+        RealVector s = new ArrayRealVector(dataS);
+        RealVector t1 = a.operate(s);
+        
+        int num_actions = actions.getRowDimension();
+        
+        //RealVector values = new ArrayRealVector(num_actions);
+        double maxValue = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < num_actions; i++) {
+        	RealVector t2 = b.operate(actions.getRowVector(i));
+        	RealVector sPrime = t1.add(t2);
+        	
+        	double value = theta.dotProduct(sPrime);
+        	if (value > maxValue) {
+        		maxIndex = i;
+        		maxValue = value;
+        	}
+        }
+        
+        RealVector selectedAction = actions.getRowVector(maxIndex);
+        
+        double velocity = selectedAction.getEntry(1);
+        double steering = selectedAction.getEntry(0);
         
         
         
