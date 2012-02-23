cfca0770553f3e10e4eafa81d7aade5f345661e7
D Mus <dmus@pc302b.liacs.nl>
2012-02-23 15:31:55 +0100

    New reward function

--- TORCS-Dynamics/reward.m
+++ TORCS-Dynamics/reward.m
@@ -1,10 +1,11 @@
 function r = reward(state)
 %REWARD Summary of this function goes here
 %   Detailed explanation goes here
-    r = state(1)^2; 
     
-    if abs(state(2)) > 1
-        r = -1;
-    end
+    % Penalize deviation from track axis
+    penalty = (state(2) / 7.5 - 1) ^2;
+    penalty = penalty + (state(3) / pi) ^2;
+    
+    r = -1 * sqrt(penalty);
 end
 
