4a8418ca7af3048b98f1ece2f45c0eccf5d2042d
dmus <derkmus@gmail.com>
2012-08-30 21:54:02 +0200

    Fixed CTE bug.

--- Simulated-Car-Racing/Dynamics/DDP/experiment.m
+++ Simulated-Car-Racing/Dynamics/DDP/experiment.m
@@ -3,6 +3,8 @@ speedControl = Laps{2}.A(:,1) + -1 * Laps{2}.A(:,2);
 U = [speedControl Laps{2}.A(:, 5) ones(size(Laps{2}.A,1),1)];
 times = computeDiscretizedTimes(Laps{2}.S);
 
+
+total_error = zeros(1,3);
 for t = 1:size(U,1)-1
     dt = times(t+1) - times(t);
     
@@ -15,4 +17,6 @@ for t = 1:size(U,1)-1
     
     % Compute squared error
     error = (Laps{2}.S(t+1,[4 1 69]) - features) .^ 2;
+    
+    total_error = total_error + error;
 end
--- Simulated-Car-Racing/Dynamics/DDP/phi.m
+++ Simulated-Car-Racing/Dynamics/DDP/phi.m
@@ -7,14 +7,17 @@ function f = phi(s, map, ref)
     trackWidth = 12;
     trackPos = ref(3) * (trackWidth / 2);
     alpha = -ref(2);
-    track = zeros(1,2);
+
     if trackPos > 0 
-        track = [abs(trackPos) * cos(alpha - 0.5*pi) abs(trackPos) * sin(alpha - 0.5*pi)];
+        track = [abs(trackPos) * cos(alpha - 0.5*pi); abs(trackPos) * sin(alpha - 0.5*pi)];
     else
-        track = [abs(trackPos) * cos(alpha + 0.5*pi) abs(trackPos) * sin(alpha + 0.5*pi)];
+        track = [abs(trackPos) * cos(alpha + 0.5*pi); abs(trackPos) * sin(alpha + 0.5*pi)];
     end
     
     index = find(map(:,1) > ref(1), 1);
+    if size(index,1) == 0
+        index = 1;
+    end
     prev_index = index - 1;
     
     if prev_index == 0
@@ -23,8 +26,13 @@ function f = phi(s, map, ref)
     distance_to_p1 = ref(1) - map(prev_index,1);
     distance_to_p2 = map(index,1) - ref(1);
     
-    p1 = (track + [distance_to_p1 * cos(alpha + pi) distance_to_p1 * sin(alpha + pi)])';
-    p2 = (track + [distance_to_p2 * cos(alpha) distance_to_p2 * sin(alpha)])';
+    temp = [distance_to_p1 * cos(alpha + pi); distance_to_p1 * sin(alpha + pi)];
+    if sum(size(track) ~= size(temp)) > 0
+        disp('error');
+    end
+    
+    p1 = track + [distance_to_p1 * cos(alpha + pi); distance_to_p1 * sin(alpha + pi)];
+    p2 = track + [distance_to_p2 * cos(alpha); distance_to_p2 * sin(alpha)];
     
     p = s(1:2);
     r = p - p1;
@@ -35,9 +43,13 @@ function f = phi(s, map, ref)
         alpha = alpha + map(prev_index,3);
         p1 = p2;
         p2 = p2 + [map(index,2) * cos(alpha); map(index,2) * sin(alpha)];
+        
+        r = p - p1;
+        dp = p2 - p1;
+        u = (r' * dp) / (dp' * dp);
+    
         index = index + 1;
         
-        %% r, dp
         if index > size(map,1)
             index = 1;
         end
@@ -51,12 +63,16 @@ function f = phi(s, map, ref)
     f = zeros(1,3);
     
     % Distance from track axis
-    f(3) = (r(2) * dp(1) - r(1) * dp(2)) / (dp' * dp);
+    % ERROR
+    % sqrt(sum(r.^2) - (r' * dp).^2)
+    relative_cte = (r(2) * dp(1) - r(1) * dp(2)) / (dp' * dp);
+    absolute_cte = relative_cte * sqrt(dp' * dp);
+    f(3) = absolute_cte / (0.5 * trackWidth);
     
     % Angle with respect to track axis
     f(2) = s(3) - (alpha + u * map(prev_index,3));
     
     % Distance to go along track axis
-    f(1) = map(index-1,1) + u * map(prev_index,2);
+    f(1) = map(prev_index,1) + u * map(prev_index,2);
 end
 
