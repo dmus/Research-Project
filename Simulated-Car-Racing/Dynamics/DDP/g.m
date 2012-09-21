78fbb1ec2aa89c35daaf4583e02c41f4208fa00f
dmus <derkmus@gmail.com>
2012-09-21 11:04:20 +0200

    Costs quadratization ready.

--- Simulated-Car-Racing/Dynamics/DDP/g.m
+++ Simulated-Car-Racing/Dynamics/DDP/g.m
@@ -2,6 +2,6 @@ function cost = g(s)
 %G Cost function (negative reward)
 %   Detailed explanation goes here
     
-
+    cost = sum(s .^ 2);
 end
 
--- Simulated-Car-Racing/Dynamics/DDP/h.m
+++ Simulated-Car-Racing/Dynamics/DDP/h.m
@@ -2,6 +2,6 @@ function cost = h(u)
 %H Summary of this function goes here
 %   Detailed explanation goes here
 
-
+    cost = 0;
 end
 
